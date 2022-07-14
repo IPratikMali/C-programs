1fb58ffee79faf5e79084078ee34bbbdec5abf27
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-14 10:40:50 +0530

    Add files via upload

---
+++ relational_opt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("%d\n",4 == 4);
+    printf("%d\n", 5 == 4);
+    printf("%d\n", 5 > 4);
+    printf("%d\n", 5 < 4);
+    printf("%d\n", 4 >= 4);
+    printf("%d\n", 5 != 4);
+    printf("%d\n", 5 != 5 );
+    return 0;
+}
