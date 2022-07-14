00b38ede58ccae4e33237d14f5221a8e96d6d1f8
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-14 10:41:44 +0530

    Add files via upload

---
+++ instructor.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+   /* int a = 10;
+    int b = a + 5 ;
+    int c = a;
+
+    printf("a is %d\n b is %d\n c is %d",a,b,c);*/
+
+    int a;
+    
+
+    printf("enter value for a");
+    scanf("%d",&a);
+
+    int b = a + 5;
+    int c = b * 6;
+    
+    printf("a is %d\n b is %d\n c is %d",a,b,c);
+    return 0;
+
+
+}
