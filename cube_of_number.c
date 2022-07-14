383a8ddc85628ed30fa348872a4bb70112a47122
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-14 10:42:14 +0530

    Add files via upload

---
+++ cube_of_number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int numb;
+    
+    printf("enter the number");
+    scanf("%d", &numb);
+    printf("cube of the given number is %d", numb * numb * numb);
+
+    return 0;
+}
