3e629f2d72cbbfe7c01201550ad7d8f85ed3bed5
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-14 10:38:51 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a , b , c;
+    printf("enter number1 ");
+    scanf("%d",&a);
+    printf("enter number2 ");
+    scanf("%d",&b);
+    printf("enter number3 ");
+    scanf("%d",&c);
+
+    printf("Average of three numbers is %d", (a+b+c)/3);
+    return 0;
+
+}
