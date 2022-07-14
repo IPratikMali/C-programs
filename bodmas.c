eb87c0844cef0bec07cf782232509f7a734162e7
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-14 10:41:05 +0530

    Add files via upload

---
+++ bodmas.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 5*2-2*3;
+    int b = 5*2/2*3;
+    int c = 5*(2/2)*3;
+    int d = 5+2/2*3;
+
+    printf("a = %d\n b = %d\n c = %d\n d = %d",a,b,c,d);
+
+    return 0;
+}
