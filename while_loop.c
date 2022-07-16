7a644aa3ba56af06d2948807e724361f7003bc50
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-16 12:59:47 +0530

    Add files via upload

---
+++ while_loop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=0 , n;;
+
+    printf("enter the number");
+    scanf("%d",&n);
+
+    while(i < n){
+        
+        printf("%d \n",i);
+        printf("hello world \n");
+        i++;
+    }
+    return 0;
+
+}
