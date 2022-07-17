b800ca128e38da57d21a7211d9a837bd6d77f0c3
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-17 10:59:17 +0530

    Add files via upload

---
+++ sum_5_to_50.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int sum= 0;
+
+    printf("sum is :");
+    for(int i =5 ; i <=50 ; i++){
+
+        sum = sum + i ;
+        
+    }
+
+    printf(" %d \n ",sum);
+    return 0;
+
+}
