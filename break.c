49fc5ceb2af20cd0b0f8ae4d6874eb6136e9af6b
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-17 11:00:21 +0530

    Add files via upload

---
+++ break.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    
+    for( int i=1 ; i<=100 ; i++){
+
+        if(i==7){
+        
+            break;
+        }
+        printf("%d \n",i);
+    }
+    return 0;
+
+}
