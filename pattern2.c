9afc19265f34dcee943feff4860e1b4bc336c8cc
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-19 20:18:32 +0530

    Add files via upload

---
+++ pattern2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int rows;
+    printf("enter rows \n");
+    scanf("%d",&rows);
+
+    for(int i = 1 ; i <= rows ; i++){
+
+        for(int j = 1 ; j <= i ; j++){
+        
+         printf(" * ");
+    }
+        
+        printf("\n");
+}
+    return 0;
+
+}
