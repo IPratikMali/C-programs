be92baca53198c3f5d66439d7ae535d4274087ce
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-19 20:18:45 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void hello();  // function declaration
+
+int main()
+{
+    hello();  // function call
+    return 0;
+
+}
+
+void hello(){
+
+    printf("hello world"); // function desfinition
+}
