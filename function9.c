ef1792a74ed33f2064e050ad95e9cf202c404382
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-20 20:28:35 +0530

    Add files via upload

---
+++ function9.c
@@ -0,0 +1,18 @@
+// print square of given number by using library functions
+
+
+#include<stdio.h>
+#include<math.h>
+
+
+
+int main()
+{
+    float numb;
+    printf("enter the number :");
+    scanf("%f",&numb);
+
+    printf("%f", pow(numb , 2));
+    return 0;
+}
+
