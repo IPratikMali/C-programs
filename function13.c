3fc65d5d21609379bc4fcb6ac9a3466ffc3b16a5
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-22 22:41:45 +0530

    Add files via upload

---
+++ function13.c
@@ -0,0 +1,26 @@
+// find the square root of given number
+
+#include<stdio.h>
+#include<math.h>
+
+
+
+int main()
+{
+    int numb;
+
+    printf("enter the number : ");
+    scanf("%d",&numb);
+
+    printf("square root is : %f",sqrt(numb));
+    return 0;
+
+}
+
+/* output
+
+enter the number : 256
+square root is : 16.000000
+
+*/
+
