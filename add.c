5d47281fe88af788312376587d142c73e8bf35e1
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-07-27 23:14:29 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a , b , sum;
+
+    printf("enter value for a");
+    scanf("%d",&a);
+    printf("enter value for b");
+    scanf("%d",&b);
+
+/* sum=a+b;
+
+    printf("addition of a and b is %d",sum ); */
+
+    printf("sum is = %d", a+b);
+
+    return 0;
+}
--- area_of_circle.c
+++ area_of_circle.c
@@ -6,6 +6,6 @@ int main()
 
     printf("enter radius of circle");
     scanf("%f",&radius);
-    printf("Area of circle is %f",radius * radius * 3.14);
+    printf("Area of circle is %f",radius * radius * 3.14); 
     return 0;
 }
