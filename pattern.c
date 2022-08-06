ca2170c8b0f5a131c2bc67d21a4e9448b8a41afc
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-08-06 19:37:39 +0530

    Add files via upload

--- pattern.c
+++ pattern.c
@@ -4,7 +4,7 @@ int main()
 {
     int rows , columns;
     printf("enter the rows and columns");
-    scanf("%d  %d",&rows,&columns);
+    scanf("%d %d",&rows,&columns);
     for(int i = 1 ; i <= rows ; i++){
         for(int j = 1 ; j <= columns ; j++){
             printf(" # ");
