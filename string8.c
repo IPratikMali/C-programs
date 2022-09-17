4f09218781439bbadd1e3ca09c90d3207fdf80c0
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-09-17 11:01:53 +0530

    Add files via upload

---
+++ string8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+    {
+        char name1[]="pratik";
+        char name2[]="prateek";
+
+        printf("%d",strcmp(name2 , name1));
+        return 0;
+
+    }
