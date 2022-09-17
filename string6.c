1192a77284af3aba9aea2a5f27d8ef7e0ca9fce0
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-09-17 11:01:29 +0530

    Add files via upload

---
+++ string6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char name1[]="pratik" ;
+    char name2[]="morgan";
+
+    strcpy(name2 , name1);
+    puts(name2);
+
+    
+    return 0;
+}
